cb0e2d05bcf5a36e7caa06c446e8423c8a9cc5a5
Samer Ibrahimovic <sameribrahimovic@hotmail.com>
2021-09-04 12:35:31 +0200

    Home page UI Changes, styles and js files added

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using AppointmentScheduling.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentScheduling.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
